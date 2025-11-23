a812f02331849ae211fb95bbb27d2850e655b07f
Nabeel N <nabeelnizam78@gmail.com>
2025-11-23 18:51:06 +0530

    -Added requirements file for package management - Updated setup.py to include new dependencies

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="COnfluence_chatbot",
+    version="0.1.0",
+    author="CIB Data Lakers",
+    author_email="nabeelnizam78@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
